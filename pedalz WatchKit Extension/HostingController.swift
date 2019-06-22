e111d62151bce75e43c85d6d74f1c438d40ed312
Jorge Moura <jjorge.moura@gmail.com>
2019-06-22 18:14:50 +0100

    Initial Commit

---
+++ pedalz WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  pedalz WatchKit Extension
+//
+//  Created by Jorge Moura on 22/06/2019.
+//  Copyright © 2019 Jorge Moura. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController : WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ pedalz WatchKit Extension/NotificationView.swift
@@ -0,0 +1,23 @@
+//
+//  NotificationView.swift
+//  pedalz WatchKit Extension
+//
+//  Created by Jorge Moura on 22/06/2019.
+//  Copyright © 2019 Jorge Moura. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct NotificationView_Previews : PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
+#endif
